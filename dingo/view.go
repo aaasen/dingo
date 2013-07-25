2045ed4b2e007a7d9ab46f64ba568b6553c1418a
Lane Aasen <laneaasen@gmail.com>
2013-07-24 18:31:04 -0700

    implements template inheritance

--- dingo/view.go
+++ dingo/view.go
@@ -10,8 +10,8 @@ type Viewer interface {
 }
 
 func RenderTemplate(w http.ResponseWriter, templateName string, data interface{}) {
-	templates := template.Must(template.ParseFiles(conf.TemplateDir + templateName))
-	err := templates.ExecuteTemplate(w, templateName, data)
+	templates := template.Must(template.ParseFiles(conf.TemplateDir+templateName, conf.TemplateDir+"base.html"))
+	err := templates.ExecuteTemplate(w, "base", data)
 
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
